4a0f885458e030635608c63dfd5fd573484131ae
SomeKidWithCode <0438786@schoolsnet.act.edu.au>
2025-02-26 09:18:27 +1100

    Set up a testing rig

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 # Imports
 import cv2 as cv
 import os
+
+# Array of tests to do
+tests = ["OCR", "RFID", "Servo"]
+
+# A loop to select a test
+def selectTest():
+    print("Select a test to run:")
+    for test in tests:
+        print(tests.index(test) + ": " + test)
+
+    selectedTest = input()
+
+    try:
+        testIndex = tests.index(selectedTest)
+        print("Selected " + selectedTest)
+        if selectedTest == "OCR":
+            OCRTest()
+        elif selectedTest == "RFID":
+            RFIDTest()
+        elif selectedTest == "Servo":
+            ServoTest()
+    except:
+        print("That is not an option")
+        selectTest()
+
+# OCR test function
+def OCRTest():
+    pass
+
+# RFID test function
+def RFIDTest():
+    pass
+
+# Servo test function
+def ServoTest():
+    pass
