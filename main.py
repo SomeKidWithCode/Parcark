9c4b33fb456bd08fc3bc959c8476a3171f3f8110
SomeKidWithCode <0438786@schoolsnet.act.edu.au>
2025-03-09 10:51:26 +1100

    'Minor changes'

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+'''
+This system is a massive mess
+Once I figure out what is and isn't nesssesary, then I can start cleaning it up
+But for now, all of this has to stay as is
+'''
+
 # Imports
-from asyncio.windows_events import NULL
-import stat
 import cv2 as cv
 import numpy as np
 
@@ -35,15 +39,16 @@ tests = ["OCR", "RFID", "Servo", "CV test"]
 
 # A loop to select a test
 def selectTest():
-    print("Select a test to run:")
+    print("Enter a test to run:")
     for test in tests:
         print(tests.index(test), ":", test)
-
-    selectedTestNum = int(input())
-
+    
     try:
+        selectedTestNum = int(input())
+
         selectedTest = tests[selectedTestNum]
         print("Selected", selectedTest)
+        print()
 
         if selectedTest == "OCR":
             OCRTest()
@@ -53,10 +58,18 @@ def selectTest():
             ServoTest()
         elif selectedTest == "CV test":
             objectDetector()
-    except Exception as e:
-        print(f"An exception occured: {e}")
+    # ValueError means that 'int(input())' failed because the provided value was not a number
+    except ValueError:
+        print(f"{selectedTestNum} is not a number")
+        selectTest()
+    # IndexError means that 'tests[selectedTestNum]' failed because the provided number was too high or low
+    except IndexError:
         print("That is not an option")
         selectTest()
+    # Anything else is just a general exception
+    except Exception as e:
+        print(f"An exception occured: {e}")
+        
 
 # OCR test function
 def OCRTest():
@@ -86,23 +99,23 @@ selectTest()
 
 # Pretrained classes in the model - Dictionary
 classNames = {
-    0: 'background',
-    1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
-    7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
-    13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
-    18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
-    24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
-    32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
-    37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
-    41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
-    46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
-    51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
-    56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
-    61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
-    67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
-    75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
-    80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
-    86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'
+    0: "background",
+    1: "person", 2: "bicycle", 3: "car", 4: "motorcycle", 5: "airplane", 6: "bus",
+    7: "train", 8: "truck", 9: "boat", 10: "traffic light", 11: "fire hydrant",
+    13: "stop sign", 14: "parking meter", 15: "bench", 16: "bird", 17: "cat",
+    18: "dog", 19: "horse", 20: "sheep", 21: "cow", 22: "elephant", 23: "bear",
+    24: "zebra", 25: "giraffe", 27: "backpack", 28: "umbrella", 31: "handbag",
+    32: "tie", 33: "suitcase", 34: "frisbee", 35: "skis", 36: "snowboard",
+    37: "sports ball", 38: "kite", 39: "baseball bat", 40: "baseball glove",
+    41: "skateboard", 42: "surfboard", 43: "tennis racket", 44: "bottle",
+    46: "wine glass", 47: "cup", 48: "fork", 49: "knife", 50: "spoon",
+    51: "bowl", 52: "banana", 53: "apple", 54: "sandwich", 55: "orange",
+    56: "broccoli", 57: "carrot", 58: "hot dog", 59: "pizza", 60: "donut",
+    61: "cake", 62: "chair", 63: "couch", 64: "potted plant", 65: "bed",
+    67: "dining table", 70: "toilet", 72: "tv", 73: "laptop", 74: "mouse",
+    75: "remote", 76: "keyboard", 77: "cell phone", 78: "microwave", 79: "oven",
+    80: "toaster", 81: "sink", 82: "refrigerator", 84: "book", 85: "clock",
+    86: "vase", 87: "scissors", 88: "teddy bear", 89: "hair drier", 90: "toothbrush"
 }
 
 # Function to return name from the dictionary
@@ -116,7 +129,9 @@ def execution_path(filename):
   return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), filename)			
 
 # Loading model
-model = cv.dnn.readNetFromTensorflow(execution_path('../OpenCV/models/frozen_inference_graph.pb'), execution_path('../OpenCV/models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt'))
+#model = cv.dnn.readNetFromTensorflow(execution_path("../OpenCV/models/frozen_inference_graph.pb"), execution_path("../OpenCV/models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt"))
+# I would imagine that the ../ thing works anyway
+model = cv.dnn.readNetFromTensorflow("../OpenCV/models/frozen_inference_graph.pb", "../OpenCV/models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt")
 
 def objectDetector():
     while True:
@@ -148,7 +163,7 @@ def objectDetector():
                 # Put some text on the bounding box
                 cv.putText(image, class_name, (int(box_x), int(box_y + .05 * image_height)), cv.FONT_HERSHEY_SIMPLEX, (.005 * image_width), (0, 0, 255))
 
-        cv.imshow('image', image)
+        cv.imshow("image", image)
 
         key = cv.waitKey(1)
 
@@ -168,9 +183,39 @@ class LPDatabase:
     def synchronize(licensePlate, time = None):
         # This may look a little convoluted so I'll explain
 
+        # If time is None, that means we are looking for a LP
+        # Then if the license plate exists in the directory, we get the value, store it, delete the entry and return it
+        # If the LP entry doesn"t exist, raise an exception
+
+        # If time isn't none, then we are adding an entry
+        # If the LP already exists in the system, then we have a duplicate-entry issue
+        # Else, we just add the value
+
+        # I am aware that this could have user-related issues but that isn't a problem at this time
+
         if time is None:
             if licensePlate in LPDatabase.lpDict:
-                return LPDatabase.lpDict[licensePlate]
-        else
-            
+                val = LPDatabase.lpDict[licensePlate]
+                LPDatabase.lpDict.pop(licensePlate)
+                return val
+            else:
+                raise Exception("Unable to fetch time from non-existent entry")
+        else:
+            if licensePlate in LPDatabase.lpDict:
+                raise Exception("A duplicate license plate entry cannot exist")
+            else:
+                LPDatabase.lpDict[licensePlate] = time
 
+    @staticmethod
+    def printDB():
+        for key in LPDatabase.lpDict:
+            print(key, LPDatabase.lpDict[key])
+
+
+# ----- Quick functions for opening and closing the boomgate ----- #
+
+def openBoomGate():
+    pass
+
+def closeBoomGate():
+    pass
