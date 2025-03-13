61c62aa3ca917257605b8a48546d3d1c825249c1
SomeKidWithCode <0438786@schoolsnet.act.edu.au>
2025-03-13 11:09:01 +1100

    E4

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PIL import Image
 from gpiozero import AngularServo
 from pirc522 import RFID
 from mfrc522 import SimpleMFRC522
-import easyocr
+#import easyocr
 
 import os, sys, inspect, pytesseract, time, signal, keyboard # type: ignore
 
@@ -22,7 +22,7 @@ servo = AngularServo(18, min_pulse_width = 0.0005, max_pulse_width = 0.0025)
 rfid = SimpleMFRC522()
 
 # Create OCR object
-ocr = easyocr.Reader(["en"])
+#ocr = easyocr.Reader(["en"])
 
 # Create camera object
 camera = cv.VideoCapture(0)
